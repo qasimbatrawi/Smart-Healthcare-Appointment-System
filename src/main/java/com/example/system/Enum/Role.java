f43648455179a04cb6f33438cd9d4f5e678da9e9
qasimbatrawi <qassim123batrawi@gmail.com>
2025-09-02 10:28:03 +0300

    Update Role

--- src/main/java/com/example/system/Enum/Role.java
+++ src/main/java/com/example/system/Enum/Role.java
@@ -1,17 +1,7 @@
 package com.example.system.Enum;
 
 public enum Role {
-    ADMIN(1),
-    DOCTOR(2),
-    PATIENT(3) ;
-
-    private final int value;
-
-    Role(int value) {
-        this.value = value;
-    }
-
-    public int getValue() {
-        return value;
-    }
+    ADMIN,
+    DOCTOR,
+    PATIENT ;
 }
